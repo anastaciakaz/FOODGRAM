e7c15937ea7612c7701b75457123544c40f0e95c
Анастасия Казанина <anastacia.kazanina@yandex.ru>
2023-06-04 21:55:13 +0700

    wf13

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,4 @@
+from api.fields import Hex2NameColor
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from recipe.models import (Favorite, Ingredient, IngredientQuantity, Recipe,
@@ -5,7 +6,6 @@ from recipe.models import (Favorite, Ingredient, IngredientQuantity, Recipe,
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.validators import UniqueTogetherValidator
-from api.fields import Hex2NameColor
 from users.models import Subscriptions, User
 
 
