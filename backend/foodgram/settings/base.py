99887b27842256ffaf715d9057b2e2bf0b31a8e4
Анастасия Казанина <anastacia.kazanina@yandex.ru>
2023-06-09 17:46:03 +0700

    db

--- backend/foodgram/settings/base.py
+++ backend/foodgram/settings/base.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY',
                        default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_VAR')
+DEBUG = os.getenv('DEBUG_VAR_F')
 
 ALLOWED_HOSTS = ['*']
 
@@ -77,6 +77,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE',
+                            default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
+    }
+}
 
 
 # Password validation
